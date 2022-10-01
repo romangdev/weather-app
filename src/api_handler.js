7e90c0a394bf05efcf52eb50b56896f4bce4b715
Roman Geathers <romangeathersdev@gmail.com>
2022-10-01 16:59:36 -0400

    Display temp for user entered city
    When user enters a city and clicks 'get weather!' button, display the
    raw data temperature for that city.

--- src/api_handler.js
+++ src/api_handler.js
@@ -1,12 +1,12 @@
 export default class ApiHandler {
   getWeatherData = async (location, dataGrabber) => {
     try {
-      let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=589970a2440f5a30f2fca88685162eb3`);
+      let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=589970a2440f5a30f2fca88685162eb3`, { mode: 'cors' });
       let data = await response.json();
-      console.log(data);
       return data;
     } catch(err) {
       alert(err);
+      console.log(err);
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,6 +4,19 @@ import ObjectMaker from './object_maker.js'
 let apiHandler = new ApiHandler();
 let objectMaker = new ObjectMaker();
 
-let data = await apiHandler.getWeatherData('london');
-let cityWeather = objectMaker.returnObject(data);
-console.log(cityWeather);
+// let cityInput = document.querySelector('.city');
+let searchBtn = document.querySelector(".search-btn");
+let weatherInfo = document.querySelector('.weather-info');
+
+searchBtn.addEventListener('click', (e) => {
+  (async function() {
+    let data = await apiHandler.getWeatherData(city.value);
+    console.log(data);
+    let cityWeather = objectMaker.returnObject(data);
+    console.log(cityWeather);
+    let temp = document.createElement('p');
+    console.log(cityWeather.temp);
+    temp.textContent = `${cityWeather.temp}`;
+    weatherInfo.append(temp);
+  })();
+});
